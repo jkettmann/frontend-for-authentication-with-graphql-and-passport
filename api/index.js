2f1a725070c4279479285ceda0462206842e44cd
Johannes Kettmann <jkettmann@gmx.net>
2019-08-02 07:52:24 +0200

    Add graphql-local and facebook strategy

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
+import 'dotenv/config';
 import express from 'express';
 import session from 'express-session';
 import uuid from 'uuid/v4';
 import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import { GraphQLLocalStrategy, buildContext } from 'graphql-passport';
 import { ApolloServer } from 'apollo-server-express';
 import User from './User';
 import typeDefs from './typeDefs';
@@ -9,6 +12,47 @@ import resolvers from './resolvers';
 
 const PORT = 4000;
 const SESSION_SECRECT = 'bad secret';
+
+const facebookOptions = {
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: 'http://localhost:4000/auth/facebook/callback',
+  profileFields: ['id', 'email', 'first_name', 'last_name'],
+};
+
+const facebookCallback = (accessToken, refreshToken, profile, done) => {
+  const users = User.getUsers();
+  const matchingUser = users.find(user => user.facebookId === profile.id);
+
+  if (matchingUser) {
+    done(null, matchingUser);
+    return;
+  }
+
+  const newUser = {
+    id: uuid(),
+    facebookId: profile.id,
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+    email: profile.emails && profile.emails[0] && profile.emails[0].value,
+  };
+  users.push(newUser);
+  done(null, newUser);
+};
+
+passport.use(new FacebookStrategy(
+  facebookOptions,
+  facebookCallback,
+));
+
+passport.use(
+  new GraphQLLocalStrategy((email, password, done) => {
+    const users = User.getUsers();
+    const matchingUser = users.find(user => email === user.email && password === user.password);
+    const error = matchingUser ? null : new Error('no matching user');
+    done(error, matchingUser);
+  }),
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -32,13 +76,16 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: 'http://localhost:4000/graphql',
+  failureRedirect: 'http://localhost:4000/graphql',
+}));
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({
-    user: req.user,
-    logout: () => req.logout(),
-  }),
+  context: ({ req, res }) => buildContext({ req, res, User }),
   playground: {
     settings: {
       'request.credentials': 'same-origin',
