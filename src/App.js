e4b2b1fbeef83d29cd5558c66d2a3772ec16b8a2
Johannes Kettmann <jkettmann@gmx.net>
2019-08-02 08:05:07 +0200

    Render current user data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 
-function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
-}
+import { CURRENT_USER_QUERY } from './queries';
+
+const App = () => {
+  const { loading, error, data } = useQuery(CURRENT_USER_QUERY);
+
+  if (loading) return <div>Loading</div>;
+  if (error) return <div>Error: {JSON.stringify(error)}</div>;
+
+  const isLoggedIn = !!data.currentUser;
+
+  if (isLoggedIn) {
+    const {
+      id,
+      firstName,
+      lastName,
+      email,
+    } = data.currentUser;
+
+    return (
+      <>
+        {id}
+        <br />
+        {firstName} {lastName}
+        <br />
+        {email}
+      </>
+    );
+  }
+
+  // SIGNUP AND LOGIN GO HERE
+  return <div>User is not logged in</div>;
+};
 
 export default App;
